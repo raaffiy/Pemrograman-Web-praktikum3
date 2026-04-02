7888f6439b55ace65faa37ca4a679285e33f0394
raaffiy <raafimaulana43@gmail.com>
2026-04-02 17:24:32 +0700

    update file

--- tugas2.php
+++ tugas2.php
@@ -1,96 +1,87 @@
 <?php
 session_start();
-
 // koneksi database
 $conn = mysqli_connect("localhost", "root", "", "AgriLens");
-
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 
-///////////////////
-////// LOGIN //////
-///////////////////
+// LOGIN 
 if (isset($_POST['login'])) {
-    $username = "admin";
-    $password = "123";
-
-    if ($_POST['username'] == $username && $_POST['password'] == $password) {
+    if ($_POST['username'] == "admin" && $_POST['password'] == "123") {
         $_SESSION['login'] = true;
     } else {
         $error = "Login gagal";
     }
 }
 
-////////////////////
-////// LOGOUT //////
-////////////////////
+// LOGOUT 
 if (isset($_GET['logout'])) {
     session_destroy();
     header("Location: " . $_SERVER['PHP_SELF']);
     exit;
 }
 
-/////////////////////////////
-////// CREATE (Tambah) //////
-/////////////////////////////
-if (isset($_POST['tambah'])) {
-    $judul = $_POST['judul'];
-    $deskripsi = $_POST['deskripsi'];
-    $date = date("Y-m-d H:i:s");
-
-    mysqli_query($conn, "INSERT INTO news (judul, deskripsi, date)
-    VALUES ('$judul', '$deskripsi', '$date')");
-
-    header("Location: " . $_SERVER['PHP_SELF']);
-    exit;
-}
-
-////////////////////
-////// DELETE //////
-////////////////////
-if (isset($_GET['delete'])) {
-    $id = $_GET['delete'];
-
-    mysqli_query($conn, "DELETE FROM news WHERE id=$id");
-
-    header("Location: " . $_SERVER['PHP_SELF']);
-    exit;
-}
-
-///////////////////////////////
-////// EDIT (AMBIL DATA) //////
-///////////////////////////////
-$editData = null;
-
-if (isset($_GET['edit'])) {
-    $id = $_GET['edit'];
-
-    $result = mysqli_query($conn, "SELECT * FROM news WHERE id=$id");
-    $editData = mysqli_fetch_assoc($result);
-}
-
-////////////////////
-////// UPDATE //////
-////////////////////
-if (isset($_POST['update'])) {
+// SIMPAN (CRUD) 
+if (isset($_POST['simpan'])) {
     $id = $_POST['id'];
     $judul = $_POST['judul'];
     $deskripsi = $_POST['deskripsi'];
 
-    mysqli_query($conn, "UPDATE news 
-        SET judul='$judul', deskripsi='$deskripsi' 
-        WHERE id=$id");
+    // upload file
+    $fileName = $_FILES['file']['name'];
+    $tmpName = $_FILES['file']['tmp_name'];
 
+    if ($fileName != "") {
+        $uploadPath = "uploads/" . $fileName;
+        move_uploaded_file($tmpName, $uploadPath);
+    }
+
+    if ($id == "") {
+        // CREATE
+        $date = date("Y-m-d H:i:s");
+        mysqli_query($conn, "INSERT INTO news (judul, deskripsi, file, date)
+        VALUES ('$judul', '$deskripsi', '$fileName', '$date')");
+    } else {
+        // UPDATE
+        if ($fileName != "") {
+            mysqli_query($conn, "UPDATE news 
+            SET judul='$judul', deskripsi='$deskripsi', file='$fileName' 
+            WHERE id=$id");
+        } else {
+            mysqli_query($conn, "UPDATE news 
+            SET judul='$judul', deskripsi='$deskripsi' 
+            WHERE id=$id");
+        }
+    }
     header("Location: " . $_SERVER['PHP_SELF']);
     exit;
+}
+
+// DELETE 
+if (isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+
+    mysqli_query($conn, "DELETE FROM news WHERE id=$id");
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit;
+}
+
+
+// EDIT (AMBIL DATA) 
+$editData = null;
+
+if (isset($_GET['edit'])) {
+    $id = $_GET['edit'];
+    $result = mysqli_query($conn, "SELECT * FROM news WHERE id=$id");
+    $editData = mysqli_fetch_assoc($result);
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Aplikasi Sederhana</title>
+    <title>Aplikasi CRUD + Upload</title>
 </head>
 
 <body>
@@ -113,32 +104,26 @@ if (isset($_POST['update'])) {
     <a href="?logout=true">Logout</a>
     <hr><br>
 
-    <!-- FORM TAMBAH / EDIT -->
-    <?php if ($editData) { ?>
-        <h3>Edit News</h3>
-        <form method="POST">
-            <input type="hidden" name="id" value="<?= $editData['id'] ?>">
+    <!-- FORM -->
+    <h3><?= $editData ? "Edit News" : "Tambah News" ?></h3>
 
-            Judul <br>
-            <input type="text" name="judul" value="<?= $editData['judul'] ?>"><br><br>
-            Deskripsi <br>
-            <textarea name="deskripsi"><?= $editData['deskripsi'] ?></textarea><br><br>
+    <form method="POST" enctype="multipart/form-data">
+        <input type="hidden" name="id" value="<?= $editData['id'] ?? '' ?>">
+        Judul <br>
+        <input type="text" name="judul" value="<?= $editData['judul'] ?? '' ?>"><br><br>
+        Deskripsi <br>
+        <textarea name="deskripsi"><?= $editData['deskripsi'] ?? '' ?></textarea><br><br>
+        Upload File <br>
+        <input type="file" name="file"><br><br>
 
-            <button type="submit" name="update">Update</button>
-        </form>
+        <?php if (!empty($editData['file'])) { ?>
+            File lama: <br>
+            <img src="uploads/<?= $editData['file'] ?>" width="100"><br><br>
+        <?php } ?>
 
-    <?php } else { ?>
-        <h3>Tambah News</h3>
-        <form method="POST">
-            Judul <br>
-            <input type="text" name="judul"><br><br>
+        <button type="submit" name="simpan">Simpan</button>
+    </form>
 
-            Deskripsi <br>
-            <textarea name="deskripsi"></textarea><br><br>
-
-            <button type="submit" name="tambah">Tambah</button>
-        </form>
-    <?php } ?>
     <hr><br>
 
     <!-- DATA -->
@@ -147,6 +132,7 @@ if (isset($_POST['update'])) {
         <tr>
             <th>Judul</th>
             <th>Deskripsi</th>
+            <th>File</th>
             <th>Date</th>
             <th>Aksi</th>
         </tr>
@@ -158,6 +144,11 @@ if (isset($_POST['update'])) {
             <tr>
                 <td><?= $p['judul'] ?></td>
                 <td><?= $p['deskripsi'] ?></td>
+                <td>
+                    <?php if ($p['file']) { ?>
+                        <img src="uploads/<?= $p['file'] ?>" width="80">
+                    <?php } ?>
+                </td>
                 <td><?= $p['date'] ?></td>
                 <td>
                     <a href="?edit=<?= $p['id'] ?>">Edit</a> |
@@ -166,6 +157,7 @@ if (isset($_POST['update'])) {
             </tr>
         <?php } ?>
     </table>
+
 <?php } ?>
 </body>
 </html>
